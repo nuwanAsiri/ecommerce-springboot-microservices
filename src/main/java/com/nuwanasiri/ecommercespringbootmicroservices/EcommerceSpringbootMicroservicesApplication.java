dd77b33e31331a124209ba1774f37f074dea9199
nuwan <lukynuwan@gmail.com>
2023-05-28 22:50:03 +0530

    initial project setup with spring initilizr

---
+++ src/main/java/com/nuwanasiri/ecommercespringbootmicroservices/EcommerceSpringbootMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.nuwanasiri.ecommercespringbootmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringbootMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringbootMicroservicesApplication.class, args);
+	}
+
+}
