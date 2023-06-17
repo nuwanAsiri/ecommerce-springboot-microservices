f518a749fc2f3c4dc9c38690c184f34cbb0b57c9
nuwanAsiri <lukynuwan@gmail.com>
2023-06-17 11:54:34 +0530

    Initial commit

---
+++ src/test/java/com/nuwanasiri/ecommercespringbootmicroservices/EcommerceSpringbootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwanasiri.ecommercespringbootmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringbootMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
